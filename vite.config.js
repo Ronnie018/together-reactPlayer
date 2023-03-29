c025d10094a4397f6b30979d88e90b09ce3933d6
= <rondinelibrt18@gmail.com>
2023-03-29 18:27:32 -0300

    change vite config to optimize gun deps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+const moduleExclude = (match) => {
+  const m = (id) => id.indexOf(match) > -1;
+  return {
+    name: `exclude-${match}`,
+    resolveId(id) {
+      if (m(id)) return id;
+    },
+    load(id) {
+      if (m(id)) return `export default {}`;
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  optimizeDeps: {
+    include: [
+      'gun',
+      'gun/gun',
+      'gun/sea',
+      'gun/sea.js',
+      'gun/lib/then',
+      'gun/lib/webrtc',
+      'gun/lib/radix',
+      'gun/lib/radisk',
+      'gun/lib/store',
+      'gun/lib/rindexed',
+    ],
+  },
+  plugins: [moduleExclude('text-encoding'), react()],
+});
